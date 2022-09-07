36a40f73fdf4499721387985b43429b1b5678987
sidoncode <siddharth54330@gmail.com>
2022-09-07 15:22:49 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringbootDemo/Component/ComponentDemo.java
@@ -0,0 +1,12 @@
+package com.example.SpringbootDemo.Component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentDemo {
+
+    public void demoFunction(){
+        System.out.println("Hello Spring in the Boots");
+    }
+
+}
---
+++ src/main/java/com/example/SpringbootDemo/SpringbootDemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.SpringbootDemo;
+
+import com.example.SpringbootDemo.Component.ComponentDemo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.example.SpringbootDemo");
+		// as we dont want the previous session , to be called from the cache memory //
+		context.refresh();
+
+		ComponentDemo componentDemo = context.getBean(ComponentDemo.class);
+
+		componentDemo.demoFunction();
+
+		context.close();
+
+	}
+
+}
